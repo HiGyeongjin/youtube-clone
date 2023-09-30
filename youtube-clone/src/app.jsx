5016989835e3e05e9e58fc1ff8e3840534d64d39
Gyeongjin Lee <asdfg031897@gmail.com>
2023-09-29 19:01:32 -0700

    commit

--- youtube-clone/src/app.jsx
+++ youtube-clone/src/app.jsx
@@ -4,7 +4,8 @@ import Guide from "./components/guide/guide";
 import SearchHeader from "./components/search_header/search_header";
 import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
-
+//실습 7 (10,11주차 유튜브클론코딩) 유튜브 데이터 api 사용하기편.
+//이건 사실 별개로 react자체를 받고싶었고 맨 마지막꺼보고 이건 삭제하거나 modify하자
 function App({ youtubeService }) {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
--- youtube-clone/src/index.js
+++ youtube-clone/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app";
+import YoutubeService from "./components/service/youtubeService";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const youtubeService = new YoutubeService(
+  process.env.REACT_APP_YOUTUBE_API_KEY
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <React.StrictMode>
+    <App youtubeService={youtubeService} />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
